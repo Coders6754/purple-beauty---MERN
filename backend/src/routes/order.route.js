28f9c63a016d40558e7484ec856ecd3bfd4e3ce8
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-25 19:17:15 +0530

    Working On Order Route Method getall

--- backend/src/routes/order.route.js
+++ backend/src/routes/order.route.js
@@ -5,6 +5,18 @@ const jwt = require("jsonwebtoken");
 const OrderModel = require("../models/order.model");
 const app = express.Router();
 
+app.get("/getall", validate, async (req, res) => {
+    const { token } = req.headers;
+    try {
+      let delivered = await OrderModel.find().populate({
+        path: "cartId",
+        populate: { path: "products", populate: "productId" },
+      });
+      return res.status(201).send({ delivered, Message: "OK" });
+    } catch (e) {
+      return res.send({ "msg": "Some thing went wrong" });
+    }
+  });
 
 
 module.exports = app;
