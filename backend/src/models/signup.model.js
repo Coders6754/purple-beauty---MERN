416a49e017373cf26ce19b0cb825b721a44c5867
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-25 14:59:06 +0530

    token condtions checked roperly

--- backend/src/models/signup.model.js
+++ backend/src/models/signup.model.js
@@ -15,6 +15,49 @@ SignupRoute.post("/", async (req, res) => {
             status: "NO",
             message: "User Already Exists, Please Signup with Different Credentials",
         });
-    } 
+    } else {
+        if (token) {
+            try {
+                const verify = jwt.verify(token, "auth@SkinCare");
+                if (verify) {
+                    if (verify.role === "Admin") {
+                        const newUser = await Auth.create({
+                            email,
+                            name,
+                            password,
+                            age,
+                            gender,
+                            address,
+                            role: "Employee",
+                            phone,
+                        });
+                        return res.status(201).json("New Employee Signedup");
+                    } else {
+                        res
+                            .status(403)
+                            .json({ status: "NO", message: "Not Authorized !!" });
+                    }
+                } else {
+                    return res.json({ status: "NO", message: "Send the Correct Token" });
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        } else {
+            const newUser = await Auth.create({
+                email,
+                name,
+                password,
+                age,
+                gender,
+                address,
+                phone,
+            });
+            return res
+                .status(201)
+                .json({ status: "OK", message: "New User Signedup" });
+        }
+    }
 });
+
 module.exports = SignupRoute;
