7db287c2014e56ece60053be0555a4fba5575016
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-25 19:06:04 +0530

    Working On Product Route

--- backend/src/routes/prod.route.js
+++ backend/src/routes/prod.route.js
@@ -3,6 +3,32 @@ const app = express.Router();
 
 const ProdModel = require("../models/product.model");
 
-
+app.get("/", async (req, res) => {
+    const { query, page = 1, limit = 20 } = req.query;
+    //   console.log(query, "t");
+  
+    try {
+      if (query) {
+        let data = await ProdModel.find({
+          product_type: { $regex: query, $options: "i" },
+        }).skip((page - 1) * limit)
+          .limit(limit);
+        if (data.length > 0) {
+          return res.send({ messg: data, state: "OK" });
+        } else {
+          let data = await ProdModel.find({
+            category: { $regex: query, $options: "i" },
+          }).skip((page - 1) * limit)
+            .limit(limit);
+          return res.send({ messg: data, state: "OK" });
+        }
+      }
+      const data = await ProdModel.find();
+      return res.send({ messg: data, state: "OK" }).skip(page - 1)
+        .limit(limit);;
+    } catch (e) {
+      return res.send({ messg: e.message, state: "NOT" });
+    }
+  });
 
 module.exports = app;
