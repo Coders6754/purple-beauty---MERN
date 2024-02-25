7c6c7179f9d5642cac3c857a07802e31f9ad9b81
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-25 14:41:07 +0530

    user route page

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express.Router();
+const UserModel = require("../models/user.model");
+const { validate } = require('../middleware/validate.middleware');
+const ProdModel = require("../models/product.model");
+app.use(validate); //it is admin validation;
+
+
+
+
+// ****************list of all users with Pagination & Sorting/Filtering*************
+app.get("/", async (req, res) => {
+    let { page = 1, limit = 5, name, address } = req.query;
+    try {
+        if (page && limit && name) {
+            let user = await UserModel.find({ name: { $regex: `${name}`, $options: "six" } });
+            res.status(200).send({ message: "OK", user });
+        } else if (page && limit && address) {
+            let user = await UserModel.find({ address: { $regex: `${address}`, $options: "six" } });
+            res.status(200).send({ message: "OK", user });
+        } else if (page && limit) {
+            if (Number(page) === 1) {
+                let user = await UserModel.find().skip(0).limit(+limit);
+                res.status(200).send({ message: "OK", user });
+            } else {
+                let s = Number(page) * Number(limit) - Number(limit);
+                let user = await UserModel.find().skip(s).limit(+limit);
+                res.status(200).send({ message: "OK", user });
+            }
+        } else {
+            const user = await UserModel.find();
+            return res.status(200).send({ message: "OK", user });
+        }
+    } catch (e) {
+        return res.send(e.message);
+    }
+});
+
+
+
+
+module.exports = app;
