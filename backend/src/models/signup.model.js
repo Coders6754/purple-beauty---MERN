6c1d804711dafa3006767d608d007a00c4e37198
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-25 14:56:26 +0530

    signup model request

--- backend/src/models/signup.model.js
+++ backend/src/models/signup.model.js
@@ -3,5 +3,8 @@ const SignupRoute = express.Router();
 const jwt = require("jsonwebtoken");
 const Auth = require("../models/user.model");
 
-
+SignupRoute.post("/", async (req, res) => {
+    const { email, password, name, age, gender, address, phone } = req.body;
+    const user = await Auth.findOne({ email });
+});
 module.exports = SignupRoute;
