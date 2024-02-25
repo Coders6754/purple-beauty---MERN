da081aeb33018e9c797471bb192c21991ab1ba55
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-25 19:16:39 +0530

    Working On Order Route Method

--- backend/src/routes/order.route.js
+++ backend/src/routes/order.route.js
@@ -1,2 +1,10 @@
+const { validate } = require('../middleware/validate.middleware');
+const express = require("express");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const OrderModel = require("../models/order.model");
+const app = express.Router();
 
 
+
+module.exports = app;
