5472ffc4dc29672a7ff3b8d005809684bd2c6fa2
TechSiren <asinha27072002@gmailo.com>
2024-02-25 14:36:14 +0530

    working on product detail

---
+++ frontend/src/Pages/Productdetails.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useEffect } from "react";
+import { SimpleGrid, Image, Box, Text, Button } from "@chakra-ui/react";
+import "./productdetails.css";
+import delivery from "../Pages/delivery.png";
+import { NavLink, useNavigate, useParams } from "react-router-dom";
+import BackendURL from "../BackendURL";
