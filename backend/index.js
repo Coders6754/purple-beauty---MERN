3d8b2f3c18a8afb44999737afe22da4ace6b551e
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-25 14:38:28 +0530

    index

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const http = require("http");
+const express = require("express");
+const app = express();
+const httpServer = http.createServer(app);
+const mongoose = require("mongoose");
+const connect = require("./src/config/db");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+mongoose.set("strictQuery", false);
+
+
+app.use(express.json());
+
+
+app.get("/", async (req, res) => {
+  res.send("yahoo!!!");
+});
+
+
+
+httpServer.listen(PORT, async () => {
+  try {
+    await connect();
+    console.log("connected to DB");
+  } catch (e) {
+    console.log({ message: e.message });
+  }
+  console.log(`Server is running at port ${PORT}`);
+});
