ed784a6e6701de48d8f408cff4344cadb316eb1e
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-25 19:19:16 +0530

    Working On Order Route Method getnotdeliveredofuser

--- backend/src/routes/order.route.js
+++ backend/src/routes/order.route.js
@@ -49,4 +49,28 @@ app.get("/getnotdelivered", async (req, res) => {
     }
   });
 
+  // Get Order details of particluar user with particular order Id
+
+app.get("/getnotdeliveredofuser/", async (req, res) => {
+    let { token } = req.headers;
+  
+    token = jwt.verify(token, process.env.token_password);
+    let userId = token.id;
+  
+    try {
+      let notDelivered = await OrderModel.find({
+        //   OrderDelivered: false ,
+        userId,
+        //   _id: id,
+      }).populate({
+        path: "cartId",
+        populate: { path: "products", populate: "productId" },
+      });
+  
+      return res.status(201).send({ notDelivered, Message: "OK" });
+    } catch (e) {
+      return res.send("Some thing went wrong");
+    }
+  });
+
 module.exports = app;
