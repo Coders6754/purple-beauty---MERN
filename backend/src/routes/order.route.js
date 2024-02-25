369c88489caa5cf0af449e763424d135d0f9df74
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-25 19:14:56 +0530

    Working On Order Model Schema Done

--- backend/src/routes/order.route.js
+++ backend/src/routes/order.route.js
@@ -1,24 +1,2 @@
 
-const OrderSchema = new Schema(
-    {
-      userId: {
-        type: Schema.Types.ObjectId,
-        ref: "user",
-        required: true,
-      },
-      cartId: {
-        type: Schema.Types.ObjectId,
-        ref: "cart",
-        required: true,
-      },
-      status: Array,
-      currentStatus: String,
-      priceTotal: Number,
-      paymentMethod: String,
-      DeliveryAdress: String,
-      OrderDelivered: { type: Boolean, default: false },
-      DeliveryDate: String,
-    },
-    { versionKey: false, timestamps: true }
-  );
 
