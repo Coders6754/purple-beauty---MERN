2aa4e32f6cd70cdb062e0f9dcd6aa1fd4cb5195d
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-25 14:57:03 +0530

    signup model conditions

--- backend/src/models/signup.model.js
+++ backend/src/models/signup.model.js
@@ -6,5 +6,9 @@ const Auth = require("../models/user.model");
 SignupRoute.post("/", async (req, res) => {
     const { email, password, name, age, gender, address, phone } = req.body;
     const user = await Auth.findOne({ email });
+
+    if (email == "" && password == "") {
+        return res.json({ status: "NO", message: "Please Fill Credentials" });
+    }
 });
 module.exports = SignupRoute;
