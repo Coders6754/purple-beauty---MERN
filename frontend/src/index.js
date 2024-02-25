056135306af675bdf8da98d49f65e91164d08028
TechSiren <asinha27072002@gmailo.com>
2024-02-25 14:00:34 +0530

    index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./Utilis/Auth";
+import { store } from "./store";
+import { Provider } from "react-redux";
+import ThemeContextProvider from './Utilis/ThemeContext/ThemeContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeContextProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <BrowserRouter>
+          <ChakraProvider>
+            <App />
+          </ChakraProvider>
+        </BrowserRouter>
+      </AuthProvider>
+    </Provider>
+  </ThemeContextProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
