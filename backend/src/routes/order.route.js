e305ab2633eed36570b847e316db135eca8d2bf5
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-25 19:13:52 +0530

    Working On Order Model Schema

--- backend/src/routes/order.route.js
+++ backend/src/routes/order.route.js
@@ -1,5 +1,3 @@
-const mongoose = require("mongoose")
-const { Schema, model } = require("mongoose");
 
 const OrderSchema = new Schema(
     {
@@ -24,7 +22,7 @@ const OrderSchema = new Schema(
     { versionKey: false, timestamps: true }
   );
 
-  
+
 const OrderModel = model("order", OrderSchema);
 
 module.exports = OrderModel;
