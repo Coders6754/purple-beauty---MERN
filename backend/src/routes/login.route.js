8b6be0bbdba1be520f7faee84a75510f64b444e2
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-25 18:54:59 +0530

    Login ROute Method Post

--- backend/src/routes/login.route.js
+++ backend/src/routes/login.route.js
@@ -1,6 +1,44 @@
 const express = require("express");
 require("dotenv").config();
 const LoginRoute = express.Router();
+const jwt = require("jsonwebtoken");
+const Auth = require("../models/user.model");
+
+var count = 0;
+LoginRoute.post("/", async (req, res) => {
+    const { email, password } = req.body;
+    var BlockedTime = Date.now();
+
+    try {
+        const user = await Auth.findOne({ email });
+        if (!user) {
+            return res.status(401).send({ message: "user not found", status: "NO" })
+        } else {
+            if (password !== user.password) {
+                count++;
+                if (count == 5) {
+                    await Auth.updateOne({ email }, { $set: { BlockedTime } });
+                }
+                return res.status(401).send({ message: "Unauthorized", status: "NO" })
+            } else {
+                const token = jwt.sign({
+                    id: user._id, role: user.role, name: user.name
+                },
+                    process.env.token_password,
+                    { expiresIn: "365d" })
+                const refreshToken = jwt.sign({
+                    id: user._id, role: user.role, name: user.name
+                },
+                    process.env.refresh_password,
+                    { expiresIn: "365d" })
+                res.status(201).json({ token, refreshToken, message: "Login Successful", status: "OK", user: user.name, role: user.role, phone: user.phone, email: user.email })
+            }
+        }
+    } catch (e) {
+        console.log(e);
+    }
+});
+
 
 
 module.exports = LoginRoute;
