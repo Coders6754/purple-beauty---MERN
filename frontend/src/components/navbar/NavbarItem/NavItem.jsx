76163c1e5f71e99bc4dd43013366c3e90b73f9b2
TechSiren <asinha27072002@gmailo.com>
2024-02-26 08:42:11 +0530

    nav bar items

--- frontend/src/components/navbar/NavbarItem/NavItem.jsx
+++ frontend/src/components/navbar/NavbarItem/NavItem.jsx
@@ -7,3 +7,27 @@ import New from "../NavItem/New";
 import Offer from "../NavItem/Offer";
 import Shop from "../NavItem/Shop";
 import Splurge from "../NavItem/Splurge";
+
+const NavItem = () => {
+  return (
+    <Flex
+      p="3px"
+      display={{ lg: "flex", md: "none", sm: "none", base: "none" }}
+      gap="30px"
+      bg="white"
+      cursor={"pointer"}
+      // boxShadow={` rgba(33, 35, 38, 0.1) 0px 10px 10px -10px;`}
+      justifyContent="center"
+    >
+      <Shop />
+      <Brand />
+      <Offer />
+      <New />
+      <Splurge />
+      <Mazine />
+      <Elite />
+    </Flex>
+  );
+};
+
+export default NavItem;
