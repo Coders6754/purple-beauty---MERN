80b8f2d4c25e5dc55dde3572276c0c6078f5d24b
TechSiren <asinha27072002@gmailo.com>
2024-02-26 08:43:27 +0530

    nav menu items

--- frontend/src/components/navbar/NavbarItem/Navmenu.jsx
+++ frontend/src/components/navbar/NavbarItem/Navmenu.jsx
@@ -41,3 +41,88 @@ let shop = [
   "Eye Liner",
   "Eye Shadow",
 ];
+function Navmenu() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
+  // let [catogoryKey, setcatogoryKey] = useState(shop);
+
+  const Navigate = useNavigate();
+  const dispatch = useDispatch();
+  // console.log(prod);
+
+  const toggleSearch = (query) => {
+    onClose();
+    dispatch(GetToQueryProduct(query));
+    Navigate("/productmain", { state: { q: "N", query } });
+    // console.log(query);
+  };
+
+  return (
+    <>
+      <Box
+        ref={btnRef}
+        onClick={onOpen}
+        color={"black"}
+        fontWeight="light"
+        fontSize="40px"
+        cursor={"pointer"}
+      >
+        <RxHamburgerMenu />
+      </Box>
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>
+            <Link to="/">
+              <AiFillHome onClick={onClose} />
+            </Link>
+          </DrawerHeader>
+
+          <DrawerBody>
+            {shop.map((item, i) => {
+              return (
+                <Box
+                  fontSize={"20px"}
+                  _hover={{
+                    fontWeight: "semibold",
+                    borderRadius: "10px",
+                  }}
+                  onClick={() => toggleSearch(item)}
+                  key={i}
+                >
+                  {item}{" "}
+                </Box>
+              );
+            })}
+          </DrawerBody>
+
+          <DrawerFooter bg="#f9f9f9">
+            <Flex
+              //   mb="50px"
+              w="100%"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              <Image
+                w="30px"
+                h="30px"
+                border="1px solid"
+                borderRadius="100%"
+                src="https://www.shutterstock.com/image-illustration/united-states-america-flag-260nw-1385203346.jpg"
+              />
+              <Text textDecor="underline">Change Language</Text>
+            </Flex>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+}
+
+export default Navmenu;
