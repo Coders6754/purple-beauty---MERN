8a12f3ca6419c3ff62956c08c98ad2a6ba21ccb5
TechSiren <asinha27072002@gmailo.com>
2024-02-26 08:40:56 +0530

    nav items

---
+++ frontend/src/components/navbar/NavbarItem/NavItem.jsx
@@ -0,0 +1,9 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+import Brand from "../NavItem/Brand";
+import Elite from "../NavItem/Elite";
+import Mazine from "../NavItem/Mazine";
+import New from "../NavItem/New";
+import Offer from "../NavItem/Offer";
+import Shop from "../NavItem/Shop";
+import Splurge from "../NavItem/Splurge";
